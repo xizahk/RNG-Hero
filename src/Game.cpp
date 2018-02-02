52ea3db44d6e38c90fc0134e1ec777146ad4b40f
jyang8 <jyang8@stuy.edu>
2018-02-02 01:02:53 -0500

    added processEvent

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,6 +1,7 @@
 // "Game.cpp"
 
 #include "Game.h"
+using namespace std;
 
 // Starts the game creates a game loop until either
 //    1) The player reaches his or her target round. OR
@@ -33,5 +34,19 @@ void Game::processShop()
 // Processes a event encounter
 void Game::processEvent()
 {
-
+	string choice;
+	double r;
+	cout << "Do you want to test your luck? Enter yes to roll a dice or no to end the round." << endl;
+	cin >> choice;
+	if (choice == "yes" || choice == "Yes") {
+		r = ((double) rand() / (RAND_MAX)) + 1;
+		if (r > 0.5) {
+			this->player.changeStat(1,HP);
+			this->player.changeStat(1,ATK);
+		}
+		else {
+			this->player.changeStat(-1,HP);
+			this->player.changeStat(-1,ATK);
+		}
+	}
 }
