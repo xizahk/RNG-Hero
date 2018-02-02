fed4dcb7c3a174256fc1362d5c19b2f383b23feb
jyang8 <jyj@mit.edu>
2018-02-02 14:16:09 -0500

    updated descriptions of fxns

--- src/Game.cpp
+++ src/Game.cpp
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <time.h>
 using namespace std;
+
 // Starts the game and creates a game loop. The loop continues until either
 //    1) The player reaches his or her target round. OR
 //    2) The player's hp reaches 0 (Game over).
@@ -57,9 +58,15 @@ void Game::processRound()
 }
 
 // Processes a fight encounter.
+//    Player will keep fighting the same enemy until:
+//       1. Player dies (i.e. Player's HP <= 0)
+//       2. Enemy dies (i.e. Enemy's HP <= 0)
+//    Player has a higher chance of fleeing from battle when:
+//       1. Player's ATK is high
+//       2. Enemy's HP is low
+//    Player and enemy's stats are displayed after fighting or fleeing
 void Game::processFight()
 {
-    
     Player temp = Player(get_round()*2, round/4, round*5);
     cout << "Player Stats: " << this->player << endl;
     cout << endl;
@@ -117,10 +124,18 @@ void Game::processFight()
         cout << "Enemy Stats: " << temp << endl;
         cout << endl;
     }
-    
 }
 
 // Processes a shop encounter
+//    Shop sells stats upgrades, each worth 10 GOLD:
+//       1. HP +1
+//       2. ATK +1
+//    Player is prompted to enter the number of upgrades to purchase for each item, respectively
+//    Once the player has inputted a valid number, the player cannot return to purchase more of the same item
+//       until the next shop encounter
+//    The number of upgrades are limited by the count of player's current GOLD
+//    The shop will notify the player of the maximum number of each item that can be purchased at this time
+//    Player's stats will be updated immediately upon purchase
 void Game::processShop()
 {
     int numHP, numATK, maxHP, maxATK, money;
@@ -136,7 +151,8 @@ void Game::processShop()
     numHP = floor(temp);
     cout << endl;
     
-    while (!cin || numHP < 0 || abs(temp - (double)numHP) > 0.01) {
+    while (!cin || numHP < 0 || abs(temp - (double)numHP) > 0.01) 
+    {
         cin.clear();
         cin.ignore(10000, '\n');
         cout << "Invalid input! How many HP +1's would you like to purchase? Currently, you can buy up to " << maxHP << endl;
@@ -146,7 +162,8 @@ void Game::processShop()
         cout << endl;
     }
     
-    while (numHP > maxHP) {
+    while (numHP > maxHP) 
+    {
         cout << "Insufficient gold! How many HP +1's would you like to purchase? Currently, you can buy up to " << maxHP << endl;
         cout << endl;
         cin >> temp;
@@ -166,7 +183,8 @@ void Game::processShop()
     numATK = floor(temp);
     cout << endl;
     
-    while (!cin || numATK < 0 || abs(temp - (double)numATK) > 0.01) {
+    while (!cin || numATK < 0 || abs(temp - (double)numATK) > 0.01) 
+    {
         cin.clear();
         cin.ignore(10000, '\n');
         cout << "Invalid input! How many ATK +1's would you like to purchase? Currently, you can buy up to " << maxATK << endl;
@@ -176,7 +194,8 @@ void Game::processShop()
         cout << endl;
     }
     
-    while (numATK > maxATK) {
+    while (numATK > maxATK) 
+    {
         cout << "Insufficient gold! How many ATK +1's would you like to purchase? Currently, you can buy up to " << maxATK << endl;
         cout << endl;
         cin >> temp;
@@ -188,8 +207,14 @@ void Game::processShop()
     this->player.changeStat(-10 * numATK,GOLD);
 }
 
-
 // Processes a event encounter
+//    Player can choose roll or not to roll
+//       Rolling will generate a random number that will affect the player's stats:
+//          1. Random number is greater than 0.5 --> HP +1, ATK +1
+//          2. Random number is less than or equal to 0.5 --> HP -1, ATK -1
+//    If the player does not roll, that signifies the end of the encounter
+//    At the end of the encounter, player will be notified of any changes to stats or the lack thereof
+//    Player's current stats will be displayed
 void Game::processEvent()
 {
     string choice;
@@ -201,16 +226,19 @@ void Game::processEvent()
     random_device rd;
     mt19937 mt(rd());
     uniform_real_distribution<double> dist(0,1);
-    if (choice == "yes" || choice == "Yes") {
+    if (choice == "yes" || choice == "Yes") 
+    {
         r = dist(mt);
-        if (r > 0.5) {
+        if (r > 0.5) 
+        {
             this->player.changeStat(1,HP);
             this->player.changeStat(1,ATK);
             cout << "Congratulations! Your stats have increased. " << endl;
             cout << "Player Stats: " << this->player << endl;
             cout << endl;
         }
-        else {
+        else 
+        {
             this->player.changeStat(-1,HP);
             this->player.changeStat(-1,ATK);
             cout << "Oh no! Your stats have decreased. " << endl;
@@ -218,7 +246,8 @@ void Game::processEvent()
             cout << endl;
         }
     }
-    else {
+    else 
+    {
         cout << "Your stats have not changed. " << endl;
         cout << "Player Stats: " << this->player << endl;
         cout << endl;
