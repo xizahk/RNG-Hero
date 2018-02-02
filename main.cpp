cffabe8675b5aaa24f97a6c54d6fea9c79e4e6ca
jyang8 <jyj@mit.edu>
2018-02-02 13:36:15 -0500

    fixed target out of range

--- main.cpp
+++ main.cpp
@@ -17,18 +17,24 @@ int main()
         Game game = Game(player);
         int target_round;
         int difficulty;
-
+        
         cout << "Please select a difficulty from 1 (easiest) to 20 (hardest): ";
         cin >> difficulty;
-
+        
+        while (difficulty < 1 || difficulty > 20)
+        {
+            cout << "Please select a difficulty from 1 to 20: ";
+            cin >> difficulty;
+        }
+        
         while (!cin)
         {
-            cout << "Please enter an integer from 1 to 5: ";
+            cout << "Please enter an integer from 1 to 20: ";
             cin >> difficulty;
         }
         target_round = difficulty * 25;
         game.start(target_round);
-
+        
         string choice;
         cout << "Would you like to play again? (Enter 'yes' to play again)" << endl;
         cin >> choice;
@@ -36,6 +42,6 @@ int main()
             play_game = false;
         }
     }
-
+    
     cout << "Thank you for playing!" << endl;
 }
